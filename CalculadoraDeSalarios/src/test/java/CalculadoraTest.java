add1c3d5bae0f6a8e64974d704930945c1f5c9b0
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2023-04-12 22:10:09 -0300

    iniciando, criando as classes

---
+++ CalculadoraDeSalarios/src/test/java/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculadoraTest {
+    @Test
+    public void testCalculoSalarioDesenvolvedor() {
+        
+    }
+}
